7b56d969d393e4a74cd04df7f687d446648e6535
Shounmay <shounmaydarsh131@gmail.com>
2022-12-24 00:55:19 +0530

    added image component

--- src/components/GalleryCard/GalleryCard.jsx
+++ src/components/GalleryCard/GalleryCard.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styles from './GalleryCard.module.css';
 
 const GalleryCard = () => {
-	return <div>GalleryCard</div>;
+	const [galleryList, setGalleryList] = useState([
+		{
+			url: 'https://images.unsplash.com/photo-1671210681777-4b7d2377ef69?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+			isLikes: false,
+			name: 'Image',
+		},
+	]);
+	return (
+		<div className={styles.imageGrid}>
+			{galleryList.map((image) => (
+				<div>
+					<img className={styles.imageCard} src={image.url} alt='' />
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default GalleryCard;
